1b0d495a3791de513c5b880518c12a348bcc34c3
Renan Gimenez <renangiimenez@gmail.com>
2026-07-06 02:37:39 -0300

    feat: implement swagger

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,11 +3,34 @@ import {
   validatorCompiler,
   serializerCompiler,
   type ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
 
 import { booksRoutes } from "./routes";
 
-export const app = fastify().withTypeProvider<ZodTypeProvider>();
+export const app = fastify({
+  routerOptions: {
+    ignoreTrailingSlash: true,
+  },
+}).withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Librarium API",
+      description: "API para gerenciamento de livros",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
